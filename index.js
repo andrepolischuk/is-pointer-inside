82314be6e04ec3f640160cb531736bfc37375787
andrepolischuk <andre.polischuk@gmail.com>
2016-05-31 23:44:30 +0300

    Add eslint

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import offset from 'global-offset';
 
-export default function isPointerInside(el, {pageX, pageY}) {
-  const {left, top} = offset(el);
-  const {offsetWidth, offsetHeight} = el;
+export default function isPointerInside(el, { pageX, pageY }) {
+  const { left, top } = offset(el);
+  const { offsetWidth, offsetHeight } = el;
 
   return pageX >= left && pageX <= left + offsetWidth &&
     pageY >= top && pageY <= top + offsetHeight;
-};
+}
